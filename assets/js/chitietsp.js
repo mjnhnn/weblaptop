1e0f4053875313a0df4fea2b77018c5e94edc288
mjnhnn <61675864+mjnhnn@users.noreply.github.com>
2020-05-12 17:26:52 +0700

    Add files via upload

---
+++ assets/js/chitietsp.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var element = document.getElementById("specifi-hide");
+    element.classList.toggle("opentab");
+
+    var an = document.getElementById("down");
+    an.classList.toggle("closetab");
+    var hien = document.getElementById("up");
+    hien.classList.toggle("opentab");
+}
+$(document).on('click', '.product-intro__btn', function() {
+    $(this).addClass('product-intro__btn--active').siblings().removeClass('product-intro__btn--active')
+})
